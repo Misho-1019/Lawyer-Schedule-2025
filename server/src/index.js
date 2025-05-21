85eed5845bb31aac6bc3a5807d2f03d6cae14162
Misho1019 <mikailtorres99@gmail.com>
2025-05-21 07:00:09 +0300

    Set up mongoose successfully

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import router from "./routes.js";
 dotenv.config();
 
 const app = express();
+
+try {
+    const uri = process.env.URI;
+    mongoose.connect(uri)
+
+    console.log('DB Connected successfully!');
+} catch (error) {
+    console.log('Cannot connect to DB!');
+    console.error(error.message);
+}
 
 app.get('/', (req, res) => {
     res.send('It works!');
