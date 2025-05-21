c14153764e37274a9706822fcc79d07886fe1bb3
Misho1019 <mikailtorres99@gmail.com>
2025-05-21 07:28:53 +0300

    Hash password on save

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
     username: {
@@ -17,6 +18,10 @@ const userSchema = new Schema({
         type: Date,
         default: Date.now,
     }
+})
+
+userSchema.pre('save', async function () {
+    this.password = await bcrypt.hash(this.password, 10)
 })
 
 const User = model('User', userSchema)
