5a4cafca0f262e084fded0c797c0bf83b814a987
Misho1019 <mikailtorres99@gmail.com>
2025-05-23 03:03:11 +0300

    Add appoinment model

---
+++ server/src/models/Appointment.js
@@ -0,0 +1,23 @@
+import { Schema, Types, model } from "mongoose";
+
+const appointmentSchema = new Schema({
+    client: {
+        type: Types.ObjectId,
+        ref: 'User',
+    },
+    date: {
+        type: String,
+    },
+    time: {
+        type: String,
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'confirmed', 'completed'],
+        default: 'pending',
+    }
+})
+
+const Appointment = model('Appointment', appointmentSchema)
+
+export default Appointment;
