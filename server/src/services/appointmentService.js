ff23260877e699a1c7ae3e5de32d62ec2d6898ec
Misho1019 <mikailtorres99@gmail.com>
2025-05-25 08:57:40 +0300

    change getByClient method to search for all appointments

--- server/src/services/appointmentService.js
+++ server/src/services/appointmentService.js
@@ -5,7 +5,7 @@ export default {
         return Appointment.find({}).sort({ date: 1, time: 1 });
     },
     getByClient(clientId) {
-        return Appointment.findById({ client: clientId })
+        return Appointment.find({ client: clientId })
     },
     create(appointmentData, creatorId) {
         return Appointment.create({
