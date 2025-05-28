da3334d551d02eb1b26e1bb6dda9d721f1da449f
Misho1019 <mikailtorres99@gmail.com>
2025-05-28 06:50:41 +0300

    Fix the model validation

--- server/src/models/Appointment.js
+++ server/src/models/Appointment.js
@@ -1,24 +1,29 @@
 import { Schema, Types, model } from "mongoose";
 
 const appointmentSchema = new Schema({
-    client: {
-        type: Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    dateTime: {
-        type: Date,
-        required: true,
-    },
-    status: {
-        type: String,
-        enum: ['pending', 'confirmed', 'completed', 'rejected'],
-        default: 'pending',
-        trim: true,
-    }
+  client: {
+    type: Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+    trim: true,
+    match: /^([01]\d|2[0-3]):([0-5]\d)$/, // Validates HH:MM in 24-hour format
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'confirmed', 'completed', 'rejected'],
+    default: 'pending',
+    trim: true,
+  }
 });
 
-
-const Appointment = model('Appointment', appointmentSchema)
+const Appointment = model('Appointment', appointmentSchema);
 
 export default Appointment;
