06ce9f0c7da57dc83ee5c1c0791883b65284b462
Misho1019 <mikailtorres99@gmail.com>
2025-06-26 05:01:16 +0300

    Add auth pages routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,10 +20,10 @@ function App() {
 
                 <Routes>
                     <Route path='/' element={ <Home /> }/>
+                    <Route path='/register' element={ <Register /> }/>
+                    <Route path='/login' element={ <Login /> }/>
                 </Routes>
-
-                {/* <Register /> */}
-                {/* <Login /> */}
+                
                 {/* <BookAppointment /> */}
                 {/* <MyAppointments /> */}
                 {/* <AppointmentConfirmation /> */}
